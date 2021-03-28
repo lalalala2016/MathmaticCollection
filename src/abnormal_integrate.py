a2d77cb1c4a3f031165df458b7955403bedae282
lalalala2016 <1795613854@qq.com>
2021-03-28 13:51:16 +0800

    反常积分

---
+++ src/abnormal_integrate.py
@@ -0,0 +1,84 @@
+# ===================================
+# @Time : 2021/3/27 12:22
+# 反常积分：无穷积分、瑕积分
+# ===================================
+from sympy import symbols, latex
+from sympy import Integral, integrate, limit, Limit
+from sympy.abc import x, y, u
+from sympy import pi, oo
+from sympy import sin, cos, tan, asin, sqrt
+from sympy.integrals.manualintegrate import integral_steps
+
+from get_integrate import analysis_steps, plot_line
+
+p = symbols('p', real=True, constant=True)
+
+
+# def limit_convergence():
+#     '''极限是否收敛'''
+def count_limit(f, a, b):
+    print('求极限：$' + latex(Limit(f, a, b)) + '$')
+    try:
+        limit(f, a, b)
+    except:
+        # 可能极限发散，没有结果吧
+        print('算不出来，和发散有关')
+
+
+def in_class():
+    '''课内习题'''
+    math_func = {}
+
+    # 等比 x^{-p}
+    p_f = 1 / (x ** p)
+    math_func['p_f'] = {'func': p_f, 'a': 1, 'b': +oo}
+
+    # 瑕积分
+    for_arcsin_f = 1 / (sqrt(1 - x ** 2))
+    math_func['for_arcsin_f'] = {'func': for_arcsin_f, 'a': 0, 'b': 1}
+    return math_func
+
+
+def analysis_indefinite_integral(f):
+    '''不定积分的计算'''
+    # 寻找原函数
+    steps = integral_steps(f, x)
+    analysis_steps(steps)
+
+def abnormal_convergence(f):
+    '''反常积分是否收敛？'''
+
+
+# def analysis_definite_integrate():
+#     '''定积分的计算'''
+
+
+math_func = in_class()
+# 反常积分载入
+f_info = math_func['for_arcsin_f']
+f = f_info['func']
+a = f_info['a']
+b = f_info['b']
+# 反常积分结果
+f_str = '$' + latex(Integral(f, (x, a, b))) + '$'
+result = '$' + latex(integrate(f, (x, a, b))) + '$'
+print('反常积分: ' + f_str + '=' + result)
+plot_line(f, a, b)
+
+
+def test():
+    # 先搞成定积分的计算，引入u
+    define_integrate = integrate(f, (x, a, u))  # 定积分计算
+    define_f_str = '$' + latex(Integral(f, (x, a, u))) + '$'
+    print('首先求定积分：' + define_f_str)
+
+    # 定积分结果
+    result_out = '$' + latex(define_integrate) + '$'
+    print('相应定积分计算：' + define_f_str + '=' + result_out)
+
+    # 如果是多个函数
+    if define_integrate.args.__len__() > 1:
+        f1 = define_integrate.args[0]
+        count_limit(f1, u, oo)
+        f2 = define_integrate.args[1]
+        count_limit(f2, u, oo)
--- src/get_integrate.py
+++ src/get_integrate.py
@@ -2,11 +2,23 @@
 # @Time : 2020/11/30 22:55
 # 求积分
 # ===================================
-
-from sympy import integrate,Integral,simplify,latex
-from sympy.abc import x,y,u,v,a,b,c,d,n,m
+from sympy import Symbol, symbols
+from sympy import integrate, Integral, simplify, latex
+from sympy.abc import x, y, u, v
 from sympy.integrals.manualintegrate import integral_steps
 from sympy import atan
+from sympy.plotting import plot, plot3d, PlotGrid
+
+
+def basement_func(steps):
+    content = steps.context
+    base_f = '$' + latex(Integral(content, x)) + '$'
+    result = '$' + latex(integrate(content, x)) + '$'
+    steps_str = str(steps)
+    if 'PowerRule' in steps_str:
+        print('基本初等函数_指数函数: ' + base_f + '=' + result)
+    if 'ArcsinRule' in steps_str:
+        print('基本初等函数_Arcsin: ' + base_f + '=' + result)
 
 
 def analysis_steps(steps):
@@ -19,29 +31,64 @@ def analysis_steps(steps):
             analysis_steps(line)
     else:
         if 'alternatives' in field:
-            print('函数变更：$'+latex(Integral(context,x))+'$')
+            print('函数变更：$' + latex(Integral(context, x)) + '$')
             new_steps = steps.alternatives[0]
-            f1 = '$'+latex(Integral(new_steps.rewritten,x))+'$'
-            print('='+f1+'\n')
+            f1 = '$' + latex(Integral(new_steps.rewritten, x)) + '$'
+            print('=' + f1 + '\n')
             analysis_steps(new_steps)
         if 'rewritten' in field:
-            new_f = '$'+latex(Integral(steps.rewritten,x))+'$'
-            print('函数Rewritten：$' + latex(Integral(context,x)) + '$ ='+new_f+'\n')
+            new_f = '$' + latex(Integral(steps.rewritten, x)) + '$'
+            print('函数Rewritten：$' + latex(Integral(context, x)) + '$ =' + new_f + '\n')
             new_steps = steps.substep.substeps
             analysis_steps(new_steps)
         if 'substeps' in field:
             new_steps = steps.substeps
             analysis_steps(new_steps)
         if 'other' in field:
-            result = simplify(integrate(context,x))
-            out4 = '$'+latex(Integral(context,x))+'='+latex(result)+'$'
-            print('分项积分：'+out4+'\n')
+            result = simplify(integrate(context, x))
+            out4 = '$' + latex(Integral(context, x)) + '=' + latex(result) + '$'
+            print('分项积分：' + out4 + '\n')
+        basement_func(steps)
 
 
+def test():
+    f_x = atan(x)
+    out_f = '$' + latex(Integral(f_x, x)) + '$'
+    steps = integral_steps(f_x, x)
+    analysis_steps(steps)
+    f_x_result = '$' + latex(simplify(integrate(f_x, x))) + '$'
+    print('结果为：' + out_f + '=' + f_x_result)
 
-f_x = atan(x)
-out_f = '$'+latex(Integral(f_x,x))+'$'
-steps = integral_steps(f_x,x)
-analysis_steps(steps)
-f_x_result = '$'+latex(simplify(integrate(f_x,x)))+'$'
-print('结果为：'+out_f+'='+f_x_result)
+
+def get_filename(f):
+    f_str = str(f)
+    if '/' in f_str:
+        f_str = f_str.replace('/', '_')
+    if '**' in f_str:
+        f_str = f_str.replace('**', '^')
+    f_str = f_str.replace(' ', '')
+    filename = '../images/' + f_str + '.jpg'
+    return filename
+
+
+def plot_line(f, a, b):
+    # 函数
+    p1 = plot(f, (x, a, b), title=f, ylabel='', show=False)
+
+    # 不定积分函数
+    indefinite_f = integrate(f, x)
+    p2 = plot(indefinite_f, (x, a, b), title=indefinite_f, ylabel='', show=False)
+    file_name = get_filename(f)
+    PlotGrid(2, 1, p1, p2).save(file_name)
+
+
+def explore_p_func():
+    a = -2
+    b = 2
+    f1 = 1 / (x ** 0.2)
+    f2 = 1 / (x ** 0.5)
+    f3 = 1 / (x ** 0.8)
+    p1 = plot(f1, (x, a, b), title=str(f1), ylabel='', show=False)
+    p2 = plot(f2, (x, a, b), title=str(f2), ylabel='', show=False)
+    p3 = plot(f3, (x, a, b), title=str(f3), ylabel='', show=False)
+    PlotGrid(3, 1, p1, p2, p3)
